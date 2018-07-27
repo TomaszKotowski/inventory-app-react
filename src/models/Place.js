624842632feed061e74cc70ddbb4ed7702cfa0c3
Chester97 <kkowalczuk27@gmail.com>
2018-07-27 15:08:27 +0200

    rename Place file

--- src/models/Place.js
+++ src/models/Place.js
@@ -1,7 +1,7 @@
 import { observable, action, computed } from "mobx";
 
 
-export default class Places {
+export default class Place {
   @observable id;
   @observable name;
   @observable officeId;
