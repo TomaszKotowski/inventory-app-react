bebfd8d0ff4f81eed7ca256ceb61fcf9930cf56b
TomaszKotowski <tomaszk72@gmail.com>
2018-07-31 12:37:28 +0200

    Minot changes to names and drawer style #17 Implement navigation

--- src/models/navigation/navigation.view.js
+++ src/models/navigation/navigation.view.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
-import { observable } from 'mobx';
 import { observer, inject } from 'mobx-react';
-import { Bind } from 'lodash-decorators';
-import { Drawer, List, NavBar, Icon, Flex, WhiteSpace } from 'antd-mobile';
+import { Drawer, NavBar, Icon, Flex } from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css';
 import './navigation.style.css'
 
@@ -18,18 +15,18 @@ export default class NavigationView extends React.Component {
         <NavBar
           rightContent={
             <Icon type="ellipsis"
-                  onClick={this.props.layoutStore.onDock}
+              onClick={this.props.layoutStore.handleDrawerDocker}
             />
           }>
           Zonifero
         </NavBar>
         <Flex direction="column" justify="start" align="start">
-          <Link to='/user' onClick={this.props.layoutStore.onDock} >PROFILE</Link>
-          <Link to='/user/:id/devices' onClick={this.props.layoutStore.onDock}>MY DEVICES</Link>
-          <Link to='/devices' onClick={this.props.layoutStore.onDock}>ALL DEVICES</Link>
-          <Link to='/employees' onClick={this.props.layoutStore.onDock}>EMPLOYEES</Link>
-          <Link to='/identify' onClick={this.props.layoutStore.onDock}>IDENTIFY</Link>
-          <Link to='/places' onClick={this.props.layoutStore.onDock}>PLACES</Link>
+          <Link to='/user' onClick={this.props.layoutStore.handleDrawerDocker}>PROFILE</Link>
+          <Link to='/user/:id/devices' onClick={this.props.layoutStore.handleDrawerDocker}>MY DEVICES</Link>
+          <Link to='/devices' onClick={this.props.layoutStore.handleDrawerDocker}>ALL DEVICES</Link>
+          <Link to='/employees' onClick={this.props.layoutStore.handleDrawerDocker}>EMPLOYEES</Link>
+          <Link to='/identify' onClick={this.props.layoutStore.handleDrawerDocker}>IDENTIFY</Link>
+          <Link to='/places' onClick={this.props.layoutStore.handleDrawerDocker}>PLACES</Link>
         </Flex>
       </div>
     );
@@ -37,10 +34,8 @@ export default class NavigationView extends React.Component {
       <Drawer
         className="my-drawer"
         style={{ minHeight: document.documentElement.clientHeight }}
-        contentStyle={{ color: '#A6A6A6', textAlign: 'center' }}
-        sidebarStyle={{ border: '1px solid #red' }}
         sidebar={sidebar}
-        docked={this.props.layoutStore.docked}
+        docked={this.props.layoutStore.drawerDocked}
         position='right'
       >
         {this.props.children}
--- src/stores/LayoutStore.js
+++ src/stores/LayoutStore.js
@@ -3,11 +3,19 @@ import { Bind } from 'lodash-decorators';
 
 class LayoutStore {
 
-  @observable docked = false;
+  @observable drawerDocked = false;
 
+  /**
+   * Function that toogles the navigation drawer from the right.
+   * To use it in the other components you have import { NavBar, Icon } from 'antd-mobile'; 
+   * add rightContent={<Icon type="ellipsis"
+   *                         onClick={this.props.layoutStore.handleDrawerDocker}
+   *                  />}
+   * to NavBar
+   */
   @Bind
-  onDock() {
-    this.docked = !this.docked;
+  handleDrawerDocker() {
+    this.drawerDocked = !this.drawerDocked;
   }
 
 };
