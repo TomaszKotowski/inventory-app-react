c462bf745bd0fda624e0dcea9d5819814a6f52c1
TomaszKotowski <tomaszk72@gmail.com>
2018-08-01 17:14:59 +0200

    Works select places from office #25

--- src/models/places/places.view.js
+++ src/models/places/places.view.js
@@ -1,37 +1,45 @@
 import React, { Component } from 'react';
-import { observable, reaction, action } from 'mobx';
+import { observable, reaction, action, autorun } from 'mobx';
 import { observer, inject } from 'mobx-react';
 import { Select } from 'antd';
-import { NavBar, Icon, Flex, Brief, WhiteSpace  } from 'antd-mobile';
+import { NavBar, Icon, Flex, Brief, WhiteSpace, List } from 'antd-mobile';
 import 'antd/dist/antd.css';
 import { bind } from 'lodash-decorators';
 import OfficesStore from '../../stores/OfficesStore';
 import PlacesStore from '../../stores/PlacesStore';
+import NavBarView from '../navigation/navigation.view';
+import './places.style.css';
 
 
 @inject('layoutStore', 'placesStore', 'officesStore')
 @observer
 export default class PlacesView extends Component {
 
+  @observable filterValue;
+  @observable office;
+
+  @bind
+  handlePlacesFilter = (value) => {
+    this.filterValue = value;
+    this.office = this.props.officesStore.officesList.find((office) => {
+      return office.id == value;
+    })
+  }
+
   componentDidMount() {
     console.log("componentDidMount");
     // console.log(this.props.officesStore.officesList)
-    console.log(this.props.placesStore.placesList)
+    reaction(
+      () => this.props.placesStore.placesList.map((place) => place.name),
+      (placeOfficeId) => { console.log(placeOfficeId) }
+    )
 
-    // const officesNames = reaction(
-    //   () => this.props.officesStore.officesList.find((office) => office),
-    //   officeName => {
-    //     console.log("He", officeName.name)
-    //   }
-    // )
-
-    // const placesNames = reaction(
-    //   () => this.props.placesStore.placesList.map((place) => place),
-    //   placeName => {
-    //     console.log("He", placeName.name)
-    //   }
-    // )
-    // console.log(officesNames())
+    const officesNames = reaction(
+      () => this.props.officesStore.officesList.map((office) => office.id),
+      officeName => {
+        console.log(officeName)
+      }
+    )
   }
 
   render() {
@@ -45,25 +53,47 @@ export default class PlacesView extends Component {
           }>
           Places
         </NavBar>
+        <WhiteSpace />
         <div>
           Officies
         </div>
         <div>
+          <WhiteSpace />
           <Select
             placeholder="Select office"
             style={{ width: `100%` }}
-            onChange={this.handleSelectOffice}>
+            onChange={this.handlePlacesFilter}>
             {
               this.props.officesStore.officesList.map(office => {
                 return (
-                  <Select.Option key={office.id} value={office.name}>{office.name}</Select.Option>
+                  <Select.Option key={office.id} value={office.id}>{office.name}</Select.Option>
                 )
               })
             }
           </Select>
         </div>
         <div>
-            
+          <Flex>
+            <List>
+              {
+                this.props.placesStore.placesList
+                  .filter((place) => place.officeId == this.filterValue)
+                  .map((place) => {
+                    return (
+                      <List.Item
+                        key={place.id}
+                        arrow="horizontal"
+                        multipleLine onClick={() => { }}
+                      >{place.name}
+                        <List.Item.Brief key={place.id} >
+                          {this.office.name}
+                        </List.Item.Brief>
+                      </List.Item>
+                    )
+                  })
+              }
+            </List>
+          </Flex>
         </div>
       </div>
     )
