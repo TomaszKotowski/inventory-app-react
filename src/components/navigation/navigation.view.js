468a905c17f7a0d72028080c0ee6f445d451f447
Maks.Zys <maks.zys.elk@gmail.com>
2018-08-02 16:43:18 +0200

    Part 2 - Conflict resolved. Fix my devices view. Fix view files directory.

---
+++ src/components/navigation/navigation.view.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { observer, inject } from 'mobx-react';
+import { Drawer, NavBar, Icon, Flex, List } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
+import './navigation.style.css';
+
+@inject('layoutStore')
+@observer
+export default class NavigationView extends React.Component {
+
+  render() {
+    const sidebar = (
+      <div>
+        <NavBar
+          rightContent={
+            <Icon type="ellipsis"
+              onClick={this.props.layoutStore.handleDrawerDocker}
+            />
+          }>
+          Zonifero
+        </NavBar>
+        <Flex direction="column" justify="start" align="start">
+        <List>
+        <List.Item key='profile'>
+          <Link to='/user' onClick={this.props.layoutStore.handleDrawerDocker}>PROFILE</Link>
+        </List.Item>
+        <List.Item key='myDevices'>
+          <Link to='/user/:id/devices' onClick={this.props.layoutStore.handleDrawerDocker}>MY DEVICES</Link>
+        </List.Item>
+        <List.Item key='allDevices'>
+          <Link to='/devices' onClick={this.props.layoutStore.handleDrawerDocker}>ALL DEVICES</Link>
+        </List.Item>
+        <List.Item key='employees'>
+          <Link to='/employees' onClick={this.props.layoutStore.handleDrawerDocker}>EMPLOYEES</Link>
+        </List.Item>
+        <List.Item key='identify'>
+          <Link to='/identify' onClick={this.props.layoutStore.handleDrawerDocker}>IDENTIFY</Link>
+        </List.Item>
+        <List.Item key='places'>
+          <Link to='/places' onClick={this.props.layoutStore.handleDrawerDocker}>PLACES</Link>
+        </List.Item>
+      </List>
+        </Flex>
+      </div>
+    );
+    return (<div>
+      <Drawer
+        className="my-drawer"
+        style={{ minHeight: document.documentElement.clientHeight }}
+        sidebar={sidebar}
+        docked={this.props.layoutStore.drawerDocked}
+        position='right'
+        enableDragHandle={false}
+      >
+        {this.props.children}
+      </Drawer>
+    </div>);
+  }
+}
---
+++ src/modules/places/places.view.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { observable, reaction, action, autorun } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import { Select } from 'antd';
+import { NavBar, Icon, Flex, Brief, WhiteSpace, List } from 'antd-mobile';
+import 'antd/dist/antd.css';
+import { bind } from 'lodash-decorators';
+import { Link, Redirect } from 'react-router-dom';
+import OfficesStore from '../../stores/OfficesStore';
+import PlacesStore from '../../stores/PlacesStore';
+import './places.style.css';
+
+
+@inject('layoutStore', 'placesStore', 'officesStore')
+@observer
+export default class PlacesView extends Component {
+
+  @observable filterOfficeId;
+  @observable office;
+
+  @bind
+  handlePlacesFilter = (optionValue) => {
+    this.filterOfficeId = optionValue;
+    this.office = this.props.officesStore.officesList.find((office) => office.id === optionValue);
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className='top-content'>
+          <NavBar
+            rightContent={
+              <Icon type="ellipsis"
+                onClick={this.props.layoutStore.handleDrawerDocker}
+              />
+            }>
+            Places
+        </NavBar>
+          <WhiteSpace />
+          <Flex justify="center" className="title-container">
+            <Flex.Item>
+              Offices
+          </Flex.Item>
+            <Link to="/places/add_device">
+              <Icon type="cross" className="cross" />
+            </Link>
+          </Flex>
+          <div>
+            <WhiteSpace />
+            <Select
+              placeholder="Select office"
+              style={{ width: `100%` }}
+              onChange={this.handlePlacesFilter}>
+              {
+                this.props.officesStore.officesList.map(office => {
+                  return (
+                    <Select.Option key={office.id} value={office.id}>{office.name}</Select.Option>
+                  )
+                })
+              }
+            </Select>
+          </div>
+        </div>
+        <div className="App">
+          <Flex>
+            <List className="places-list">
+              {
+                this.props.placesStore.placesList
+                  .filter((place) => place.officeId === this.filterOfficeId)
+                  .map((place) => {
+                    return (
+                      <List.Item
+                        key={place.id}
+                        arrow="horizontal"
+                        multipleLine
+                        onClick={() => { }}
+                      >{place.name}
+                        <List.Item.Brief key={place.id} >
+                          {this.office.name}
+                        </List.Item.Brief>
+                      </List.Item>
+                    )
+                  })
+              }
+            </List>
+          </Flex>
+        </div>
+      </div>
+    )
+  }
+};
