00ee33eedc309f6d7db29f24a3ad7225f0917b92
TomaszKotowski <tomaszk72@gmail.com>
2018-08-02 11:41:49 +0200

    Some variables name chenged and added minor styles to places.view #25

--- src/models/places/places.view.js
+++ src/models/places/places.view.js
@@ -5,10 +5,9 @@ import { Select } from 'antd';
 import { NavBar, Icon, Flex, Brief, WhiteSpace, List } from 'antd-mobile';
 import 'antd/dist/antd.css';
 import { bind } from 'lodash-decorators';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import OfficesStore from '../../stores/OfficesStore';
 import PlacesStore from '../../stores/PlacesStore';
-import NavBarView from '../navigation/navigation.view';
 import './places.style.css';
 
 
@@ -16,17 +15,19 @@ import './places.style.css';
 @observer
 export default class PlacesView extends Component {
 
-  @observable filterValue;
+  @observable filterOfficeId;
   @observable office;
 
   @bind
-  handlePlacesFilter = (value) => {
-    this.filterValue = value;
-    this.office = this.props.officesStore.officesList.find((office) => office.id == value);
+  handlePlacesFilter = (optionValue) => {
+    this.filterOfficeId = optionValue;
+    this.office = this.props.officesStore.officesList.find((office) => office.id === optionValue);
   }
 
+  //  remove later
   componentDidMount() {
     console.log("componentDidMount");
+
     reaction(
       () => this.props.placesStore.placesList.map((place) => place.name),
       (placeOfficeId) => { console.log(placeOfficeId) }
@@ -43,44 +44,46 @@ export default class PlacesView extends Component {
   render() {
     return (
       <div>
-        <NavBar
-          rightContent={
-            <Icon type="ellipsis"
-              onClick={this.props.layoutStore.handleDrawerDocker}
-            />
-          }>
-          Places
+        <div className='top-content'>
+          <NavBar
+            rightContent={
+              <Icon type="ellipsis"
+                onClick={this.props.layoutStore.handleDrawerDocker}
+              />
+            }>
+            Places
         </NavBar>
-        <WhiteSpace />
-        <Flex justify="center" className="title-container">
-          <Flex.Item>
-            Officies
-          </Flex.Item>
-          <Link to="/places/add_device">
-            <Icon type="cross" className="cross" />
-          </Link>
-        </Flex>
-        <div>
           <WhiteSpace />
-          <Select
-            placeholder="Select office"
-            style={{ width: `100%` }}
-            onChange={this.handlePlacesFilter}>
-            {
-              this.props.officesStore.officesList.map(office => {
-                return (
-                  <Select.Option key={office.id} value={office.id}>{office.name}</Select.Option>
-                )
-              })
-            }
-          </Select>
+          <Flex justify="center" className="title-container">
+            <Flex.Item>
+              Offices
+          </Flex.Item>
+            <Link to="/places/add_device">
+              <Icon type="cross" className="cross" />
+            </Link>
+          </Flex>
+          <div>
+            <WhiteSpace />
+            <Select
+              placeholder="Select office"
+              style={{ width: `100%` }}
+              onChange={this.handlePlacesFilter}>
+              {
+                this.props.officesStore.officesList.map(office => {
+                  return (
+                    <Select.Option key={office.id} value={office.id}>{office.name}</Select.Option>
+                  )
+                })
+              }
+            </Select>
+          </div>
         </div>
-        <div>
+        <div className="App">
           <Flex>
-            <List>
+            <List className="places-list">
               {
                 this.props.placesStore.placesList
-                  .filter((place) => place.officeId == this.filterValue)
+                  .filter((place) => place.officeId === this.filterOfficeId)
                   .map((place) => {
                     return (
                       <List.Item
--- src/routers/devices.router.js
+++ src/routers/devices.router.js
@@ -6,7 +6,7 @@ const DevicesRouter = ({ match }) => (
     <Route exact path='/devices' render={() => <div>devices</div>} />
     <Route path="/devices/add_device" render={() => <div>devices add device</div>} />
     <Route exact path="/devices/:id" render={({ match }) => <div>devices: {match.params.id}</div>} />
-    <Route exact path="/devices}/:id/transfer" render={({ match }) => <div>devices: {match.params.url}</div>} />
+    <Route exact path="/devices/:id/transfer" render={({ match }) => <div>devices: {match.params.url}</div>} />
   </Switch>
 );
 
