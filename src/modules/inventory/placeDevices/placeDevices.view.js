4fea290e0826d4d2c4c76510d5e31c6f42f0e5cb
TomaszKotowski <tomaszk72@gmail.com>
2018-08-06 13:05:35 +0200

    Styled the module 'placeDevices' #27

---
+++ src/modules/inventory/placeDevices/placeDevices.view.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { observable, reaction } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import 'antd/dist/antd.css';
+import { Flex } from 'antd-mobile';
+import { List } from 'antd';
+import { bind } from 'lodash-decorators';
+import PlacesStore from '../../../stores/PlacesStore';
+import DevicesStore from '../../../stores/DevicesStore';
+import NavBarView from '../../../Components/navigation/navBar.view';
+import './placeDevices.style.css'
+
+@inject('placesStore', 'devicesStore')
+@observer
+export default class PlaceDevices extends Component {
+
+  @observable id = `4cb4d5f6-1477-40b9-b855-bac9a2192b3c`
+  @observable place
+  @observable deviceList
+
+  componentDidMount() {
+    console.log('Did mount');
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='top-content'>
+          <NavBarView title="Place's Devices" />
+          <Flex direction="column" align="center" justify="center" >
+            <Flex.Item>
+              Place name
+            </Flex.Item>
+            <Flex.Item>
+              Devices:
+            </Flex.Item>
+          </Flex>
+        </div>
+        <div className='app-content'>
+          <Flex direction='column' align='center' justify='center'>
+            <Flex.Item className='list-item'>
+              <List className='list' >
+                {
+                  this.props.devicesStore.devicesList
+                    .filter((device) => device.belongsToId == this.id)
+                    .map((device) => {
+                      return (
+                        <List.Item
+                          key={device.id}
+                          arrow="horizontal"
+                          multipleLine
+                          onClick={() => { }}
+                        >{device.name}
+                        </List.Item>
+                      )
+                    })
+                }
+              </List>
+            </Flex.Item>
+          </Flex>
+        </div>
+      </div>
+    )
+  }
+};
+
--- src/routers/places.router.js
+++ src/routers/places.router.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import PlacesView from '../models/places/places.view';
+import PlacesView from '../modules/inventory/places/places.view';
+import PlaceDevices from '../modules/inventory/placeDevices/placeDevices.view'
+import AddPlace from '../modules/addPlace/addPlace.view';
 
 const PlacesRouter = ({ match }) => (
   <Switch>
     <Route exact path="/places" component={PlacesView} />
-    <Route path="/places/add_device" render={({ match }) => <div>places: {match.url}</div>} />
-    <Route path="/places/:id" render={({ match }) => <div>places: {match.url}</div>} />
+    <Route exact path="/places/:id" component={PlaceDevices} />
+    <Route path="/places/add" component={AddPlace} />
+    <Route path="/places/:id" render={({ match }) => <div>places: {match.params.id}</div>} />
   </Switch>
 );
 
