96fbc66b78dc1baf0f0a3af676f189ab5dc632aa
Maks.Zys <maks.zys.elk@gmail.com>
2018-08-09 17:11:40 +0200

    Nothing changed

--- src/modules/deviceTransfer/deviceTransfer.js
+++ src/modules/deviceTransfer/deviceTransfer.js
@@ -9,7 +9,7 @@ import QrReader from "../../components/qrCode/reader/qrReader";
 const Item = List.Item;
 const Brief = Item.Brief;
 
-@inject('devicesStore','layoutStore')
+@inject('devicesStore', 'layoutStore')
 @observer
 class DeviceTransfer extends React.Component {
   @observable device = {};
@@ -46,26 +46,26 @@ class DeviceTransfer extends React.Component {
     console.log(this.props.match.params.id);
     return (
       <Flex direction="column" align="stretch">
-      <Flex.Item>    
-      <Flex.Item align="center">
-          <NavBar
-            rightContent={<Icon type="ellipsis" onClick={this.props.layoutStore.handleDrawerDocker} />}>
-            My Devices
+        <Flex.Item>
+          <Flex.Item align="center">
+            <NavBar
+              rightContent={<Icon type="ellipsis" onClick={this.props.layoutStore.handleDrawerDocker} />}>
+              My Devices
           </NavBar>
-      </Flex.Item>
-      <Link to="/devices">
-          <Item arrow="horizontal" multipleLine onClick={() => {}}>
-            {this.device.name}
-          </Item>
-      </Link>
-      </Flex.Item>
-      <Flex.Item align="center" className="device-targed-box1">
-            <div>CHOOSE TARGET</div>
+          </Flex.Item>
+          <Link to="/devices">
+            <Item arrow="horizontal" multipleLine onClick={() => { }}>
+              {this.device.name}
+            </Item>
+          </Link>
+        </Flex.Item>
+        <Flex.Item align="center" className="device-targed-box1">
+          <div>CHOOSE TARGET</div>
         </Flex.Item>
         <Flex.Item align="center" className="device-targed-box2" justify="center;">
-          <QrReader deviceId={this.device.id} transfer={true} reject={this.reject} accept={this.accept}/>
+          <QrReader deviceId={this.device.id} transfer={true} reject={this.reject} accept={this.accept} />
         </Flex.Item>
-    </Flex> 
+      </Flex>
     );
   }
 }
