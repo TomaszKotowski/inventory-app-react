89a9ab5cb82e4f70dd15696da35916a0a4ddcf88
Chester97 <kkowalczuk27@gmail.com>
2018-08-10 10:46:44 +0200

    add pencil to profile view

--- src/modules/profile/ProfileImage.js
+++ src/modules/profile/ProfileImage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import mobx from 'mobx';
 import { Flex, WhiteSpace } from 'antd-mobile';
+import { Icon } from 'antd';
+import { Link } from 'react-router-dom';
 import UsersStore from '../../stores/UsersStore';
 import { inject, observer } from 'mobx-react';
 import './profileImage.css';
@@ -15,6 +17,11 @@ class ProfileImage extends Component {
     return(
       <div>
         <WhiteSpace size="xl"/>
+        <Flex justify='end'>
+          <Link to="">
+          <Icon type="edit" style={{ fontSize: 26, color: '#08c', marginRight:40 }} />
+          </Link>
+        </Flex>
         <Flex justify='center'>
           <img className='avatar' src={this.props.userStore.getPictureAvatarLink} alt="User avatar"/>
         </Flex>
